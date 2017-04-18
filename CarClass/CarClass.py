8e0dc993104cd96f6a617e0a6b9e0700bd651db8
Muhwezi Allan <Muhwezi Allan>
2017-04-18 18:02:31 +0300

    added support for encapsulation

--- CarClass/CarClass.py
+++ CarClass/CarClass.py
@@ -1,35 +1,43 @@
-class CarClass(object):
+class Car(object):
     """A car class that can be used to instantiate various vehicles"""
 
     # Defaults to General and GM if no arguments are provided
-    def __init__(self, name='General', model='GM', vehicle_type='saloon'):
-        self.name = name
-        self.model = model
-        self.vehicle_type = vehicle_type
+    def __init__(self, name='General', model='GM', type='saloon'):
+        self.__name = name
+        self.__model = model
+        self.__type = type
         self.speed = 0
 
         # Number of doors 
-        if self.name == 'Porshe' or self.name == 'Koenigsegg':
+        if self.__name == 'Porshe' or self.__name == 'Koenigsegg':
             self.num_of_doors = 2
         else:
             self.num_of_doors = 4
 
         # Number of Wheels 
-        if self.vehicle_type == 'trailer':
+        if self.__type == 'trailer':
             self.num_of_wheels = 8
         else:
             self.num_of_wheels = 4
 
+    @property
+    def model(self):
+        return self.__model
+
+    @property
+    def name(self):
+        return self.__name
+
     # Saloon Car 
     def is_saloon(self):
-        if self.vehicle_type == 'saloon':
+        if self.__type == 'saloon':
             return True
         else:
             return False
 
     # Car Speed  
-    def drive(self, desired_speed):
-        if self.vehicle_type == 'trailer':
+    def drive_car(self, desired_speed):
+        if self.__type == 'trailer':
             self.speed = desired_speed * 11
             return self
         else:
@@ -37,4 +45,3 @@ class CarClass(object):
                 self.speed = 10 ** desired_speed
             else:
                 self.speed = 10 * desired_speed
-                
